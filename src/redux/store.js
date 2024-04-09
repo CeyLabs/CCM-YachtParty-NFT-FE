08649340b7038f47bf0fd46dbd653be6fc421b74
kiruththigan <kiru1997kiruthi@gmail.com>
2024-04-09 16:07:18 +0530

    delete all files

--- src/redux/store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import accountProfileReducer from "./accountProfileSlice";
-
-export const store = configureStore({
-  reducer: {
-    accountProfile: accountProfileReducer,
-  },
-});
