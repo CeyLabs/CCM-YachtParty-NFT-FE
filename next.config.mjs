17cf1bcc471052844c2ec4edefb77235cc585212
kiruththigan <kiru1997kiruthi@gmail.com>
2024-04-09 15:53:33 +0530

    nextjs setup

---
+++ next.config.mjs
@@ -0,0 +1,12 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  webpack: (config) => {
+    config.externals.push("pino-pretty", "lokijs", "encoding");
+    return config;
+  },
+};
+
+// next.config.mjs
+export default nextConfig;
+
--- views/HomeView.tsx
+++ views/HomeView.tsx
@@ -1,50 +1,312 @@
-import React from "react";
-import { Breadcrumb, Image, Layout, Menu, theme } from "antd";
-import { CustomConnectionButton } from "../components/CustomConnectionButton";
+"use client";
 
-const { Header, Content, Footer } = Layout;
-
-const items = new Array(15).fill(null).map((_, index) => ({
-  key: index + 1,
-  label: `nav ${index + 1}`,
-}));
+import MintingCard from "@/components/home/MintingCard";
+import { Card, Checkbox, Col, Image, Row, Space } from "antd";
+import Link from "next/link";
+import React, { useEffect, useState } from "react";
+import { useAccount, useChainId } from "wagmi";
+import axios from "axios";
 
 const HomeView: React.FC = () => {
+  const { address } = useAccount();
+  const chainId = useChainId();
+
+  const [walletAgeChecked, setWalletAgeChecked] = useState(false);
+  const [walletAgeDisabled, setWalletAgeDisabled] = useState(true);
+
+  const [PortfolioBalanceChecked, setPortfolioBalanceChecked] = useState(false);
+  const [PortfolioBalanceDisabled, setPortfolioBalanceDisabled] =
+    useState(true);
+
+  const [holdingNftsChecked, setHoldingNftsChecked] = useState(true);
+  const [holdingNftsDisabled, setHoldingNftsDisabled] = useState(true);
+
+  const [profileInBTC, setProfileInBTC] = useState(0);
+
+  const getProfileInBTC = async () => {
+    try {
+      const options = {
+        headers: {
+          accept: "application/json",
+          authorization:
+            "Basic emtfZGV2X2JkNzgxMGMzMTIwMjQ0NDdiZDZhYWE5NDg3YjE0ZTg3Og==",
+        },
+      };
+      const response = await axios.get(
+        `https://api.zerion.io/v1/wallets/${`${address}`}/portfolio?currency=btc`,
+        options
+      );
+
+      if (response.status === 200) {
+        setProfileInBTC(
+          response?.data?.data?.attributes?.positions_distribution_by_type
+            ?.wallet
+        );
+      }
+      console.log(
+        "response btc",
+        response?.data?.data?.attributes?.positions_distribution_by_type?.wallet
+      );
+    } catch (error) {
+      console.log("Error while fetching profile", error);
+    }
+  };
+
+  useEffect(() => {
+    if (address) {
+      getProfileInBTC();
+    }
+  }, [address, chainId]);
+
+  useEffect(() => {
+    if (profileInBTC > 1) {
+      setPortfolioBalanceChecked(true);
+      setPortfolioBalanceDisabled(false);
+    } else {
+      setPortfolioBalanceChecked(false);
+      setPortfolioBalanceDisabled(true);
+    }
+  }, [profileInBTC]);
+
   return (
-    <Layout
-      style={{
-        minHeight: "100vh",
-      }}
-    >
-      <Header
-        style={{
-          display: "flex",
-          alignItems: "center",
-          height: "62px",
-          background: "#000000",
-          borderBottom: "1px solid #222",
-        }}
-      >
-        <div
+    <div style={{marginBottom:"20px"}}>
+      <Row>
+        <Col
+          className="col-hidden"
+          xs={24}
+          sm={24}
+          md={7}
+          lg={7}
+          xl={8}
+          xxl={8}
           style={{
             display: "flex",
-            justifyContent: "space-between",
+            justifyContent: "flex-end",
             alignItems: "center",
-            width: "100%",
+            padding: "5px",
+            marginTop: "20px",
           }}
         >
-          {/* <Image
-            preview={false}
-            src="https://gw.alipayobjects.com/zos/antfincdn/FLrTNDvlna/antvr.png"
-          /> */}
-          <CustomConnectionButton />
-        </div>
-      </Header>
-      <Content style={{ padding: "0 48px" }}></Content>
-      <Footer style={{ textAlign: "center", background: "#111111" }}>
-        Ant Design ©{new Date().getFullYear()} Created by Ant UED
-      </Footer>
-    </Layout>
+          <Space direction="vertical" size={30}>
+            <div
+              style={{
+                display: "flex",
+                alignItems: "center",
+                flexDirection: "row",
+                gap: "5px",
+              }}
+            >
+              <Image
+                src="/images/point.svg"
+                alt="logo"
+                width={20}
+                height={20}
+              />
+              <div style={{ fontSize: "16px" }}>
+                Sri Lanka’s First yacht Party
+              </div>
+            </div>
+            <div
+              style={{
+                display: "flex",
+                alignItems: "center",
+                flexDirection: "row",
+                gap: "5px",
+              }}
+            >
+              <Image
+                src="/images/point.svg"
+                alt="logo"
+                width={20}
+                height={20}
+              />
+              <div style={{ fontSize: "16px" }}>
+                Sri Lanka’s First yacht Party
+              </div>
+            </div>
+            <div
+              style={{
+                display: "flex",
+                alignItems: "center",
+                flexDirection: "row",
+                gap: "5px",
+              }}
+            >
+              <Image
+                src="/images/point.svg"
+                alt="logo"
+                width={20}
+                height={20}
+              />
+              <div style={{ fontSize: "16px" }}>
+                Sri Lanka’s First yacht Party
+              </div>
+            </div>
+          </Space>
+        </Col>
+        <Col
+          xs={24}
+          sm={24}
+          md={10}
+          lg={10}
+          xl={8}
+          xxl={8}
+          style={{ padding: "5px", marginTop: "20px" }}
+        >
+          <h1
+            style={{
+              fontSize: "32px",
+              fontWeight: "bold",
+              textAlign: "center",
+              marginBottom: "20px",
+            }}
+          >
+            Mint Your NFT
+          </h1>
+          <MintingCard />
+        </Col>
+        <Col
+          xs={24}
+          sm={24}
+          md={7}
+          lg={7}
+          xl={8}
+          xxl={8}
+          style={{
+            display: "flex",
+            justifyContent: "flex-start",
+            alignItems: "center",
+            padding: "5px",
+            marginTop: "20px",
+          }}
+        >
+          <Space direction="vertical" size={30}>
+            <div
+              style={{
+                display: "flex",
+                alignItems: "center",
+                flexDirection: "row",
+                gap: "5px",
+              }}
+            >
+              <Checkbox checked={walletAgeChecked} disabled={walletAgeDisabled}>
+                Wallet Aged more than (XXX) Days
+              </Checkbox>
+            </div>
+            <div
+              style={{
+                display: "flex",
+                alignItems: "center",
+                flexDirection: "row",
+                gap: "5px",
+              }}
+            >
+              <Checkbox
+                checked={PortfolioBalanceChecked}
+                disabled={PortfolioBalanceDisabled}
+              >
+                Portfolio Balance is More than 1 BTC ({profileInBTC} BTC)
+              </Checkbox>
+            </div>
+            <div
+              style={{
+                display: "flex",
+                alignItems: "center",
+                flexDirection: "row",
+                gap: "5px",
+              }}
+            >
+              <Checkbox
+                checked={holdingNftsChecked}
+                disabled={holdingNftsDisabled}
+              >
+                Holding the{" "}
+                <Link
+                  target="_blank"
+                  href="https://opensea.io/collection/ceylon-crypto-meetup"
+                >
+                  Genesis CCM NFT
+                </Link>{" "}
+                (25% Offer)
+              </Checkbox>
+            </div>
+          </Space>
+        </Col>
+        <Col
+          className="col-show-mobile"
+          xs={24}
+          sm={24}
+          md={7}
+          lg={7}
+          xl={8}
+          xxl={8}
+          style={{
+            display: "none",
+            justifyContent: "flex-end",
+            alignItems: "center",
+            padding: "5px",
+            marginTop: "20px",
+          }}
+        >
+          <Space direction="vertical" size={30}>
+            <div
+              style={{
+                display: "flex",
+                alignItems: "center",
+                flexDirection: "row",
+                gap: "5px",
+              }}
+            >
+              <Image
+                src="/images/point.svg"
+                alt="logo"
+                width={20}
+                height={20}
+              />
+              <div style={{ fontSize: "16px" }}>
+                Sri Lanka’s First yacht Party
+              </div>
+            </div>
+            <div
+              style={{
+                display: "flex",
+                alignItems: "center",
+                flexDirection: "row",
+                gap: "5px",
+              }}
+            >
+              <Image
+                src="/images/point.svg"
+                alt="logo"
+                width={20}
+                height={20}
+              />
+              <div style={{ fontSize: "16px" }}>
+                Sri Lanka’s First yacht Party
+              </div>
+            </div>
+            <div
+              style={{
+                display: "flex",
+                alignItems: "center",
+                flexDirection: "row",
+                gap: "5px",
+              }}
+            >
+              <Image
+                src="/images/point.svg"
+                alt="logo"
+                width={20}
+                height={20}
+              />
+              <div style={{ fontSize: "16px" }}>
+                Sri Lanka’s First yacht Party
+              </div>
+            </div>
+          </Space>
+        </Col>
+      </Row>
+    </div>
   );
 };
 
