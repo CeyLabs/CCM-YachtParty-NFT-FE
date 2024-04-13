f145eea9524f2261cf57beb495b15d8283fc2ac6
Sarani Ranasinghe <sara.ranasinghe@gmail.com>
2024-04-14 00:59:08 +0530

    Fix UI: footer emoji remove

--- components/home-page/footer.tsx
+++ components/home-page/footer.tsx
@@ -5,7 +5,7 @@ import Image from "next/image";
 const FooterBar: React.FC = () => {
     return (
         <div className="sticky mx-auto text-xs text-slate-400 flex h-16 gap-1 items-center x-4 md:px-6">
-            Made in Sri Lanka 🇱🇰 Developed by <a target="_blank" href="https://perfectustec.com/">Perfectus</a> 🧬
+            Made in Sri Lanka 🇱🇰 Developed by <a target="_blank" href="https://perfectustec.com/">Perfectus</a>
         </div>     
     );
   }
